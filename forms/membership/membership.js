b85e9f4c299a3494b2b8e6345471f9a89f38607f
gak77901 <gak77901@creighton.edu>
2021-09-29 16:09:58 -0500

    Membership

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,14 @@
-/*
-let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+let names = ""
+
+btnSubmit.onclick=function(){
+  names = InptName.value
+  if (members.includes(names)) {
+  lblName.value = names + ", you are already a member."
+} else {
+  members.push(names)
+  lblName.value = names + ", you have been added to the membership roster."
+  }
+}
